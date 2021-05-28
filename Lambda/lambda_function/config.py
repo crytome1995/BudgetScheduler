edbb53cdddad436a7fd404b15797bed08db42af8
crytome1995 <ethan.lebioda@gmail.com>
2021-05-27 21:13:20 -0500

    Adding boilter plate functions

--- Lambda/lambda_function/config.py
+++ Lambda/lambda_function/config.py
@@ -2,21 +2,19 @@ import os
 
 
 class ConfigDefaults:
-    LAMBDA_RULE_NAME = 'budget_reporter'
+    LAMBDA_RULE_NAME = 'testfunction'
     DEFAULT_AWS_REGION = 'us-east-1'
-
-
 
 class Configuration(ConfigDefaults):
 
     def __init__(self,
                  lambda_rule_name=ConfigDefaults.LAMBDA_RULE_NAME,
-                 default_region=ConfigDefaults.DEFAULT_AWS_REGION
+                 default_region=ConfigDefaults.DEFAULT_AWS_REGION,
                  ):
 
         self._lambda_rule_name = lambda_rule_name
         self._default_region = default_region
-
+        self._username = ""
 
     @property
     def lambda_rule_name(self):
@@ -26,5 +24,12 @@ class Configuration(ConfigDefaults):
     def default_region(self):
         return self._default_region
 
+    @property
+    def username(self):
+        return 'ethan'
+    
+    @username.setter
+    def username(self, u):
+        self._username = u
 
 config = Configuration()
--- Lambda/lambda_function/create_cw_event.py
+++ Lambda/lambda_function/create_cw_event.py
@@ -1,21 +1,58 @@
 import boto3
 from config import config
-
 import os
+import json
 
-os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+os.environ["AWS_DEFAULT_REGION"] = config.default_region
 client = boto3.client('events')
+lambda_client = boto3.client('lambda')
+
+USERNAME_KEY = "userName"
+
 def create_cloudwatch_event(event, context): 
-    x = "ethan"
+    config.username = event[USERNAME_KEY]
+    print(config.username)
 
 
-def build_cloudwatch_event(eventName, scheduleExpression):
+def build_cloudwatch_event(eventName):
+    # Every SUNDAY
+    scheduleExpression = "cron(0 12 ? * SUN *)"
     client.put_rule(
         Name=eventName,
         ScheduleExpression=scheduleExpression,
         State='DISABLED'
     )
 
+def build_cloudwatch_event_target(eventName):
+    lambda_arn = get_lambda_arn()
+    lambda_input = get_input_for_lambda()
+    client.put_targets(
+        Rule=eventName,
+        Targets=[
+            {
+                'Id': eventName,
+                'Arn': lambda_arn,
+                'Input': lambda_input
+            }
+        ]
+    )
 
+def get_input_for_lambda():
+    '''
+    Build the input that will be passed to the lambda function
+    '''
+    lambda_input = {}
+    lambda_input[USERNAME_KEY] = config.username
+    return json.dumps(lambda_input)
 
-build_cloudwatch_event("test","cron(0 0 ? * SUN *)")
+def get_lambda_arn():
+    '''
+    based on the name of the lambda function return the associated ARN of the function
+    '''
+    alias_dict = lambda_client.get_function(
+        FunctionName=config.lambda_rule_name
+    )
+    #ARN of the function
+    return alias_dict['Configuration']['FunctionArn']
+
+build_cloudwatch_event_target('testevent')
